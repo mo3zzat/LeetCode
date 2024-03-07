d392d4a98ed1259dd62c4db15f328422f95bb1d0
mo3zzat <152310077+mo3zzat@users.noreply.github.com>
2024-03-07 14:45:35 +0200

    Time: 0 ms (100.00%), Space: 40.9 MB (23.30%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum = n * (n+1) / 2;
+        int leftSum = 0;
+        for(int i = 1 ; i<= n; i++){
+            leftSum += i;
+            if(sum - leftSum + i  == leftSum) return i;
+        }
+        return -1;
+    }
+}
