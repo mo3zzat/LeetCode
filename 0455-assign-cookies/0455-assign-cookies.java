9729bf38cd0354094c84e962a7f212eec73ab89c
mo3zzat <152310077+mo3zzat@users.noreply.github.com>
2024-03-07 02:30:37 +0200

    Time: 9 ms (46.19%), Space: 45 MB (80.32%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = g.length - 1, j = s.length - 1;
+        int c = 0;
+        while(i >= 0 && j >= 0){
+            if(g[i] <= s[j]){
+                j--;
+                c++;
+            }
+            i--;
+        }
+
+        return c;
+    }
+}
