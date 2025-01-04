fccd0210666454c7305fcc9909a6a2df1be75552
Mohamed Ezzat <152310077+mo3zzat@users.noreply.github.com>
2025-01-05 01:03:24 +0200

    Time: 3 ms (29.93%), Space: 41.9 MB (60.51%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        List<Integer> ls = new ArrayList<>();
-        Arrays.sort(arr);
-        for(int i = 0 ;i < arr.length  ; i++){
-            int cnt = 1;
-            while (i + 1 < arr.length && arr[i] == arr[i+1]  ){
-                cnt++;
-                i++;
-            }
-            if(ls.contains(cnt)){
-                return false;
-            }
-            ls.add(cnt);
-
+        Map<Integer,Integer> mp = new HashMap<>();
+        Set<Integer> st = new HashSet<>();
+        for(int num : arr){
+            mp.put(num, mp.getOrDefault(num,1) + 1);
         }
-        return true;
+        for(int num : mp.keySet()){
+            st.add(mp.get(num));
+        }
+        return st.size() == mp.size();
     }
 }
