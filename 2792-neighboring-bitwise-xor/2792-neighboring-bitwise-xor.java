030d0f656955f999de147e34650343ad44c8a7b0
Mohamed Ezzat <152310077+mo3zzat@users.noreply.github.com>
2025-01-17 11:13:00 +0200

    Time: 560 ms (6.47%), Space: 62.6 MB (26.47%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int sum = 0;
+        for(int num : derived)
+            sum ^= num;
+        return sum == 0;
+    }
+}
